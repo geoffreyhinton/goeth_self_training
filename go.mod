e0d46b027de256ce8fce0742ce9ab8b9d21b5cb3
Geoffrey Hinton <geoffreyhinton1305@gmail.com>
2025-06-09 11:29:31 +0700

    Split up db and trie and added interface

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/geoffreyhinton/goeth_self_training
 
 go 1.22.5
+
+require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+)
---
+++ trie.go
@@ -0,0 +1,49 @@
+package main
+
+type Database interface {
+	Put(key, value []byte)
+	Get(key []byte) ([]byte, error)
+}
+
+type Trie interface {
+	root string
+	db   Database
+}
+
+func NewTrie(db Database) *Trie {
+	return &trie{db: db, root: ""}
+}
+
+func (t *Trie) Update(key string, value string) {
+	k := CompactHexDecode(key)
+  
+	t.root = t.UpdateState(t.root, k, value)
+}
+
+func (t *Trie) Get(key []byte) ([]byte, error) {
+	return nil, nil
+}
+
+// Inserts a new sate or delete a state based on the value
+func (t *Trie) UpdateState(node string, key []int, value string) string {
+	if value != "" {
+	  return t.InsertState(node, ""/*key*/, value)
+	} else {
+	  // delete it
+	}
+  
+	return ""
+}
+
+func (t *Trie) InsertState(node, key, value string) string {
+	return ""
+}
+
+func (t *Trie) Put(node []byte) []byte {
+	enc := Encode(node)
+	sha := Sha256Bin(enc)
+  
+	t.db.Put([]byte(sha), enc)
+  
+	return sha
+}
