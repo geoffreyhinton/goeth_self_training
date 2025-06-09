3e28bc0d4988e4bccc9ffbd5b8c5f99a0202be2d
Geoffrey Hinton <geoffreyhinton1305@gmail.com>
2025-06-09 14:37:01 +0700

    VM Recovery

--- block_manager.go
+++ block_manager.go
@@ -10,7 +10,7 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 )
 
 type BlockManager struct {
@@ -32,7 +32,13 @@ func (bm *BlockManager) ProcessBlock(block *Block) error {
 
 	// Process each transaction/contract
 	for _, tx := range block.transactions {
-		go bm.ProcessTransaction(tx, block, lockChan)
+		// If there's no recipient, it's a contract
+		if tx.recipient == "" {
+			go bm.ProcessContract(tx, block, lockChan)
+		} else {
+			// "finish" tx which isn't a contract
+			lockChan <- true
+		}
 	}
 
 	// Wait for all Tx to finish processing
@@ -43,14 +49,22 @@ func (bm *BlockManager) ProcessBlock(block *Block) error {
 	return nil
 }
 
-func (bm *BlockManager) ProcessTransaction(tx *Transaction, block *Block, lockChan chan bool) {
-	if tx.recipient == "\x00" {
-		bm.vm.RunTransaction(tx, block, func(opType OpType) bool {
-			// TODO calculate fees
+func (bm *BlockManager) ProcessContract(tx *Transaction, block *Block, lockChan chan bool) {
+	// Recovering function in case the VM had any errors
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from VM execution with err =", r)
+			// Let the channel know where done even though it failed (so the execution may resume normally)
+			lockChan <- true
+		}
+	}()
 
-			return true // Continue
-		})
-	}
+	// Process contract
+	bm.vm.ProcContract(tx, block, func(opType OpType) bool {
+		// TODO calculate fees
+
+		return true // Continue
+	})
 
 	// Broadcast we're done
 	lockChan <- true
