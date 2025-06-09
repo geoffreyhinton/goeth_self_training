215b20c578aaeafebbb33c98fcb1c9620af9fabb
Geoffrey Hinton <geoffreyhinton1305@gmail.com>
2025-06-09 12:18:06 +0700

    Added helper methods for slice comparison and nibble matching

--- util.go
+++ util.go
@@ -1,23 +1,47 @@
 package main
 
 import (
-  "strconv"
-  "crypto/sha256"
-  "encoding/hex"
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
 )
 
 func Uitoa(i uint32) string {
-  return strconv.FormatUint(uint64(i), 10)
+	return strconv.FormatUint(uint64(i), 10)
 }
 
 func Sha256Hex(data []byte) string {
-  hash := sha256.Sum256(data)
+	hash := sha256.Sum256(data)
 
-  return hex.EncodeToString(hash[:])
+	return hex.EncodeToString(hash[:])
 }
 
 func Sha256Bin(data []byte) []byte {
-  hash := sha256.Sum256(data)
+	hash := sha256.Sum256(data)
 
-  return hash[:]
+	return hash[:]
+}
+
+// Helper function for comparing slices
+func CompareIntSlice(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func MatchingNibbleLength(a, b []int) int {
+	i := 0
+	for CompareIntSlice(a[:i+1], b[:i+1]) && i < len(b) {
+		i += 1
+	}
+
+	//fmt.Println(a, b, i-1)
+
+	return i
 }
